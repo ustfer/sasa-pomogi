50b8ae1388dd7601d35ff92cc72ad18abe846780
baka <baka@baka.baka>
2019-06-02 15:32:03 +0300

    init

---
+++ database/factories/CalendarFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use App\Calendar;
+use Faker\Generator as Faker;
+
+$factory->define(Calendar::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
