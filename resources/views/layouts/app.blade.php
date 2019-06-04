31cf0f518eafcb345816ea7ec377625c84a13678
baka <baka@baka.baka>
2019-06-04 12:38:53 +0300

    sasa2

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,7 +25,7 @@
     <div id="app">
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <div class="container">
-                <a class="navbar-brand" href="#">Школа 31</a>
+                <a class="navbar-brand" href="#">Дент-Реал</a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse"
                     data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
                     aria-label="Toggle navigation">
